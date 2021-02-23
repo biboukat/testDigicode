d2b64102100c896f3c7b3307212633088020513f
alexander kovtun <kovtuna@mydicode.com>
2021-02-23 10:20:55 +0200

    init

---
+++ data/videos.ts
@@ -0,0 +1,12 @@
+export const videoList: string[] = [
+  'https://www.sampleposts.com/wp-content/uploads/2020/10/Never-Trust-Too-Much-Funny-Whatsapp-Status-Video.mp4?_=5',
+  'https://www.sampleposts.com/wp-content/uploads/2020/10/Short-Funny-WhatsApp-Status-Video-Wrong-Parking.mp4?_=1',
+  'https://www.sampleposts.com/wp-content/uploads/2020/10/Funny-Drunk-Goat-WhatsApp-Status-video.mp4?_=2',
+  'https://www.sampleposts.com/wp-content/uploads/2020/10/Funny-Whatsapp-Video-Status-Be-Carefull-Choosing-Dress.mp4?_=3',
+  'https://www.sampleposts.com/wp-content/uploads/2020/10/Funny-Whatsapp-Status-Video-Prank-Goes-Wrong.mp4?_=4',
+  'https://www.sampleposts.com/wp-content/uploads/2020/10/Me-on-Day-Before-Exam-Funny-Whatsapp-Status-Video.mp4?_=6',
+  'https://www.sampleposts.com/wp-content/uploads/2020/10/Thats-Why-God-Created-Men-Funny-Whatsapp-Status-Video.mp4?_=7',
+  'https://www.sampleposts.com/wp-content/uploads/2020/10/Funny-Fat-men-Funny-Status-Video.mp4?_=8',
+  'https://www.sampleposts.com/wp-content/uploads/2020/10/Petty-Shop-Encounter-Funny-Whatsapp-Status-Video.mp4?_=9',
+  'https://www.sampleposts.com/wp-content/uploads/2020/10/Aeroplane-Dance-Funny-Whatsapp-Status-Video.mp4?_=10',
+];
